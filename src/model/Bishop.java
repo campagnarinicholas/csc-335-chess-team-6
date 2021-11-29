41901d9e531fecc788df147901265e839725d928
mearroyo33 <mearroyo33@email.arizona.edu>
2021-11-28 18:41:53 -0700

    Refactored color to be an integer

--- src/model/Bishop.java
+++ src/model/Bishop.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class Bishop extends Piece {
 
-	public Bishop(int x, int y, String color, ArrayList<ArrayList<Piece>> board) {
+	public Bishop(int x, int y, int color, ArrayList<ArrayList<Piece>> board) {
 		super(x, y, color, board);
 	}
 
